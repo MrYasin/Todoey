abdf5e4ea696dd9ed2b91a4db71bbf469920b563
MrYasin <yasin@iltercengiz.info>
2019-09-20 22:15:06 +0300

    Fixed whitespaces

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,32 +12,18 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        print(dataFilePath)
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "ASDdd"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "QEWE"
-        itemArray.append(newItem3)
-
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
+        loadItems()
     
     }
 
     //MARK: - TableView DataSource Methods
-    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -64,18 +50,21 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
-//      When a row is tapped gets rid of gray flashing
-        tableView.deselectRow(at: indexPath, animated: true)
         
 //      If .done property is true it becomes false vice versa
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        tableView.reloadData()
+        saveItems()
+        
+//      When a row is tapped gets rid of gray flashing
+        tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
-    
+
     
     //MARK: - Add New Item
+
+    
     
     @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -85,30 +74,16 @@ class TodoListViewController: UITableViewController {
         // POP-UP FOR ADDING ITEM
         
         let alert = UIAlertController(title: "Add New ToDo", message: "", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-
-        // WHEN ADD ITEM BUTTON PRESSED
+        let action = UIAlertAction(title: "Add Item", style: .default) { [weak self] (action) in
+            guard let self = self else { return }
+            // WHEN ADD ITEM BUTTON PRESSED
             
             let newItem = Item()
-            newItem.title = textField.text!
-            
-            if textField.text != nil {
-                self.itemArray.append(newItem)
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")  // saves the data so when app is terminated and relaunched it shows up using Line 22 function
-                self.tableView.reloadData()  // Reload data so added items show up
-            } else {
-//                self.itemArray.append("New Item")
-//                self.defaults.set(self.itemArray, forKey: "TodoListArray")
-//                self.tableView.reloadData()  // Reload data so added items show up
-            }
-            
+            newItem.title = self.title(with: textField.text)
+            self.itemArray.append(newItem)
+            self.saveItems()
         }
-        
-//        CANCEL BUTTON
-        
-//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
-//            print("cancelled")
-//        }
+
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
             print("Cancelled")
         }))
@@ -129,8 +104,85 @@ class TodoListViewController: UITableViewController {
     }
     
     
+    //MARK: - Model Manupilation Methods
+    
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()  // Reload data so added items show up
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
+    
+    // Add item text field
+    
+    func title(with entry: String?) -> String {
+        if let title = entry?.trimmingCharacters(in: .whitespacesAndNewlines), title.count > 0 {
+            return title
+        }
+        return "New Item"
+    }
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
